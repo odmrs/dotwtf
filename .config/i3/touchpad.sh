8b2a55e1790d9dc45a7f0708f7654db6619be11d
odmrs <omarcosviniciusdev@gmail.com>
2024-10-31 07:25:05 -0300

    feat: add i3wm config

---
+++ .config/i3/touchpad.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+xinput set-prop "DELL0B26:00 04F3:3147 Touchpad" "libinput Tapping Enabled" 1
+xinput set-prop "DELL0B26:00 04F3:3147 Touchpad" "libinput Natural Scrolling Enabled" 1
+xinput set-prop "DELL0B26:00 04F3:3147 Touchpad" "libinput Disable While Typing Enabled" 1
+xinput set-prop "DELL0B26:00 04F3:3147 Touchpad" "libinput Click Method Enabled" 0, 1
+
